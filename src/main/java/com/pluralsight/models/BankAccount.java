225ed7826a13eb144b2e7b1f9af7d8683b27b111
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-03 10:12:45 -0500

    * created BankAccount Class, initialized variables, getters, and other methods for class

---
+++ src/main/java/com/pluralsight/models/BankAccount.java
@@ -0,0 +1,49 @@
+package com.pluralsight.models;
+
+public class BankAccount {
+    private String name, accountNumber;
+    private double balance;
+
+    public BankAccount(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public double getValue() {
+        return this.balance;
+    }
+
+}
