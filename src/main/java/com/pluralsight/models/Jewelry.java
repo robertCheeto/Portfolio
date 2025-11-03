f204f1e81c60c8bcd5ac7d42d17b67ce01b7c4ca
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-03 10:06:05 -0500

    * created Jewelry Child Class, initialized variable and overrided getMarketValue()

---
+++ src/main/java/com/pluralsight/models/Jewelry.java
@@ -0,0 +1,19 @@
+package com.pluralsight.models;
+
+public class Jewelry extends FixedAsset {
+    private double karat;
+
+    public Jewelry(String name, double marketValue, double karat) {
+        super(name, marketValue);
+        this.karat = karat;
+    }
+
+    public double getKarat() {
+        return karat;
+    }
+
+    @Override
+    public double getMarketValue() {
+        return super.getMarketValue();
+    }
+}
