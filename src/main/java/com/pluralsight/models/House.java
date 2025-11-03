906564b37312745d4414bf2648635654dc9f4376
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-03 10:08:43 -0500

    * created House Child Class, initialized variables, getters, and overrided getMarketValue()

---
+++ src/main/java/com/pluralsight/models/House.java
@@ -0,0 +1,29 @@
+package com.pluralsight.models;
+
+public class House extends FixedAsset {
+    private int yearBuilt, squareFeet, bedrooms;
+
+    public House(String name, double marketValue, int yearBuilt, int squareFeet, int bedrooms) {
+        super(name, marketValue);
+        this.yearBuilt = yearBuilt;
+        this.squareFeet = squareFeet;
+        this.bedrooms = bedrooms;
+    }
+
+    public int getYearBuilt() {
+        return yearBuilt;
+    }
+
+    public int getSquareFeet() {
+        return squareFeet;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    @Override
+    public double getMarketValue() {
+        return super.getMarketValue();
+    }
+}
